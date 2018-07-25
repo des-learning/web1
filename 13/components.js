a313874088baa29b62f13b22b56a7619ef12b982
Des Dulianto <desdulianto@gmail.com>
2018-07-25 18:57:10 +0700

    emit event

--- 13/components.js
+++ 13/components.js
@@ -5,8 +5,13 @@ Vue.component('my-table', {
       <tr>\
         <td v-for="column in columns">{{column}}</td>\
       </tr>\
-      <tr v-for="row in rows">\
+      <tr v-for="row in rows" @click="click(row)">\
         <td v-for="column in columns">{{row[column]}}</td>\
       </tr>\
-     </table>'
+     </table>',
+  methods: {
+    click: function(row) {
+      this.$emit('tekan', {data: row})
+    }
+  }
 })
